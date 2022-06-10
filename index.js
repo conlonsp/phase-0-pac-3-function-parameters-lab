3068666d6c04a0c590b455b965d143e097108a8c
samuelconlon <conlonsp@yahoo.com>
2022-06-10 19:52:48 -0400

    commit tree 99be6615b83a88634248f580114a526763c8ab28

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(name1) {
+    return `Hi, my name is ${name1}.`
+}
+function introductionWithLanguage(name1, language1) {
+    return `Hi, my name is ${name1} and I am learning to program in ${language1}.`
+}
+function introductionWithLanguageOptional(name1, language1 = "JavaScript") {
+    return `Hi, my name is ${name1} and I am learning to program in ${language1}.`
+}
+console.log(Sam)
+function introductionWithLanguageOptional(name1, language1 = "JavaScript") {
+    return `Hi, my name is ${name1} and I am learning to program in ${language1}.`
+}
+console.log(Sam, Python)
